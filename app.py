a09711271493fecab778fa320331f9538981ce8b
Gerson Aranibar <aranibargerson28@gmail.com>
2021-10-27 22:33:50 -0500

    update app

--- app.py
+++ app.py
@@ -4,4 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    
